c0001e0551a32f4005ee412352f32ba24dcb1f0f
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-07-17 01:47:11 +0800

    add spec_to_xyz function

--- matlab/spec_to_xyz.m
+++ matlab/spec_to_xyz.m
@@ -1,20 +1,16 @@
-function rgb = spec_to_rgb(spec, varargin)
+function xyz = spec_to_xyz(spec, varargin)
 % DESCRIPTION
-%   This function converts spectra data into RGB colors.
+%   This function converts spectra data into XYZ colors.
 % SYNTAX
-%   rgb = spec_to_rgb(spec);
-%   rgb = spec_to_rgb(..., Name, Value...);
+%   xyz = spec_to_rgb(spec);
+%   xyz = spec_to_rgb(..., Name, Value...);
 % INPUT
 %   spec:               n*2 matrix. It records (wavelength, power density) pair.
 %                       Wavelength is in nm.
 % PARAMETER
-%   'ColorSpace':     	A string for colorspace. Default is 'sRGB'.
-%                       see internal.cs_name_validator for detail.
 %   'Mixed':            Whether to recognize input spectrum as single color. Default is true.
 %                       If set to false, each row in spec will result a color, representing
 %                       a pure spectral color.
-%   'Clamping':         A string for RGB clamping method. Default is 'Clip'.
-%                       see internal.rgb_clamping_validator for detail.
 %   'Y':                Expected output luminance (Y component). Default is 1.0.
 %                       If 'Mixed' is set to false, then 'Y' is the max lumincance.
 % OUTPUT
@@ -23,12 +19,20 @@ function rgb = spec_to_rgb(spec, varargin)
 
 p = inputParser;
 p.addRequired('spec', @(x) validateattributes(x, {'numeric'}, {'2d', 'ncols', 2}));
-p.addParameter('ColorSpace', 'sRGB', @internal.cs_name_validator);
 p.addParameter('Mixed', true, @(x) validateattributes(x, {'logical'}, {'scalar'}));
-p.addParameter('Clamping', 'DeSat', @internal.rgb_clamping_validator);
 p.addParameter('Y', 1, @(x) validateattributes(x, {'numeric'}, {'scalar'}));
 p.parse(spec, varargin{:});
 
-xyz = spec_to_xyz(spec, 'Mixed', p.Results.Mixed, 'Y', p.Results.Y);
-rgb = colorspace.xyz2rgb(xyz, p.Results.ColorSpace, p.Results.Clamping);
+cmf = internal.xyz_cmf();
+
+if p.Results.Mixed
+    spd = interp1(spec(:, 1), spec(:, 2), cmf(:, 1), 'linear', 0);
+    xyz = bsxfun(@times, cmf(:, 2:end), spd);
+    xyz = sum(xyz);
+else
+    cmf = interp1(cmf(:, 1), cmf(:, 2:end), spec(:, 1), 'linear', 0);
+    xyz = bsxfun(@times, cmf, spec(:, 2));
+end
+
+xyz = xyz / max(xyz(:, 2)) * p.Results.Y;
 end
