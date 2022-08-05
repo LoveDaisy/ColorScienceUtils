5d245ea974c5f41b0e2299a7138b914af775a15c
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-08-05 16:19:03 +0800

    add rgb2yuv

--- matlab/+colorspace/ycbcr2rgb.m
+++ matlab/+colorspace/ycbcr2rgb.m
@@ -2,8 +2,6 @@ function rgb = ycbcr2rgb(yuv, varargin)
 % DESCRIPTION
 %   It converts YCbCr color to RGB color, with given colorspace parameter
 %   (may get from colorspace.get_param).
-%   It will not take transfer characteristics into account. Generally it takes
-%   non linear yuv signal in, and produces non linear rgb signal out.
 % SYNTAX
 %   rgb = ycbcr2rgb(yuv)
 %   rgb = ycbcr2rgb(yuv, yuv_name)
@@ -11,13 +9,15 @@ function rgb = ycbcr2rgb(yuv, varargin)
 %   rgb = ycbcr2rgb(..., target_rgb_name)
 %   rgb = ycbcr2rgb(..., target_rgb_param)
 % INPUT
-%   yuv:                n*3 array, each row represents a color.
+%   yuv:                n*3 array, each row represents a color. uv components are in range [-0.5, 0.5].
 %   yuv_name:           A string for colorspace name. Default is '709'.
 %                       See internal.cs_name_validator for detail.
 %   yuv_param:          A struct returned from colorspace.get_param.
 %   target_rgb_name:    A string for colorspace name. Default is 'sRGB'.
 %                       See internal.cs_name_validator for detail.
 %   target_rgb_param:   A struct returned from colorspace.get_param.
+% OUTPUT
+%   rgb:                n*3 array, each row represents a color.
 
 p = inputParser;
 p.addRequired('yuv', @(x) validateattributes(x, {'numeric'}, {'2d', 'ncols', 3}));
@@ -43,7 +43,7 @@ coef_cr = yuv_param.yuv(5);
 % Construct the matrix:
 %   yuv = rgb * m;
 m = [coef_y; ([0, 0, 1] - coef_y) / coef_cb; ([1, 0, 0] - coef_y) / coef_cr]';
-rgb = (yuv - [0, 0.5, 0.5]) / m;
+rgb = yuv / m;
 
 % Then convert to target RGB space
 rgb = colorspace.rgb2rgb(rgb, yuv_param, rgb_param);
