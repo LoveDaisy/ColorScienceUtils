ca9de92b0af2a386933a2713c30ae6be528b1893
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-08-17 01:35:02 +0800

    remove unnecessary optional input argument

--- matlab/+colorspace/xyz2lab.m
+++ matlab/+colorspace/xyz2lab.m
@@ -1,15 +1,10 @@
-function [lab, max_y] = xyz2lab(xyz, varargin)
+function [lab, max_y] = xyz2lab(xyz)
 % DESCRIPTION
 %   Convert XYZ color to Lab color space. Reference white point is D65.
 % SYNTAX
 %   lab = xyz2lab(xyz)
-%   lab = xyz2lab(xyz, high_cut)
 % INPUT
 %   xyz:            n*3 matrix, each row represents a color; or m*n*3 for 3-channel image.
-%   high_cut:       A scalar. For HDR image, y compnent may be greater than 1.0, and often the max
-%                   luminance is extremely high (say, a specular highlight). We can cut off
-%                   some extreme values higher than high_cut to keep better representation for
-%                   major colors. It is in percent. Default is 100.
 % OUTPUT
 %   lab:            Same shape to input xyz. For normal image, L ranges between [0, 100], and
 %                   ab ranges mainly in [-0.5, 0.5].
@@ -19,16 +14,14 @@ input_size = size(xyz);
 
 p = inputParser;
 p.addRequired('xyz', @colorutil.image_shape_validator);
-p.addOptional('high', 100, @isnumeric);
 p.parse(xyz);
 
 w = colorspace.get_white_point('D65');
 xyz = reshape(xyz, [], 3) ./ w;
 max_y = max(xyz(:, 2));
 if max_y > 1.001
-    % HDR/linear scene light.
-    max_y = prctile(xyz(:, 2), p.Results.high);
-    y2 = min(xyz(:, 2) ./ max_y, 1.0);  % extreme values are cut to prctile(y, high)
+    % HDR case or linear scene light.
+    y2 = xyz(:, 2) ./ max_y;
     xyz = xyz .* (y2 ./ xyz(:, 2));
 else
     % SDR image. Y ranges in [0, 1]
