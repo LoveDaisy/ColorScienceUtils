7f1b25d3e0753cbc16cbad9893a5b6f4ba8b3994
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-08-16 09:24:25 +0800

    update plot_yuv
    add alternitive input arguments

--- matlab/+colorvis/plot_yuv.m
+++ matlab/+colorvis/plot_yuv.m
@@ -1,28 +1,58 @@
-function plot_yuv(y, u, v, varargin)
+function plot_yuv(varargin)
 % DESCRIPTION
 %   It displays YCbCr data properly. If not specified, it shows uv components as half size as y
 %   component, and place them side by side along the long side of y component.
 % SYNTAX
 %   plot_yuv(y, u, v);
+%   plot_yuv(data);
 %   plot_yuv(..., Name, Value...);
 % INPUT
 %   y:          m*n array. Range in [0, 1].
 %   u, v:       m/2*n/2 array (for 420) or m*n/2 array (for 422) or m*n array (for 444).
 %               Range in [-0.5, 0.5].
+%   data:       A struct with field yfp, ufp, vfp
 % OPTIONS
 %   All options for imshow().
+
+input_arg_yuv = isnumeric(varargin{1});
+p = inputParser;
+if input_arg_yuv
+    p.addRequired('y', @(x) validateattributes(x, {'numeric'}, {'2d'}));
+    p.addRequired('u', @(x) validateattributes(x, {'numeric'}, {'2d'}));
+    p.addRequired('v', @(x) validateattributes(x, {'numeric'}, {'2d'}));
+    p.parse(varargin{1:3});
+    if length(varargin) > 3
+        varargin = varargin(4:end);
+    else
+        varargin = {};
+    end
+else
+    p.addOptional('data', [], @(x) isstruct(x) && isfield(x, 'yfp') && isfield(x, 'ufp') && isfield(x, 'vfp'));
+    p.parse(varargin{1});
+    if length(varargin) > 1
+        varargin = varargin(2:end);
+    else
+        varargin = {};
+    end
+end
+
+if ~input_arg_yuv
+    y = p.Results.data.yfp;
+    u = p.Results.data.ufp;
+    v = p.Results.data.vfp;
+else
+    y = p.Results.y;
+    u = p.Results.u;
+    v = p.Results.v;
+end
 
 y_size = size(y);
 u_size = size(u);
 v_size = size(v);
-
-p = inputParser;
-p.addRequired('y', @(x) validateattributes(x, {'numeric'}, {'2d'}));
-p.addRequired('u', @(x) all(size(x) == v_size) && ...
-    (size(x, 1) == y_size(1) || size(x, 1) == y_size(1) / 2) && ...
-    (size(x, 2) == y_size(2) || size(x, 2) == y_size(2) / 2));
-p.addRequired('v', @(x) all(size(x) == u_size));
-p.parse(y, u, v);
+if any(u_size ~= v_size) || any(~(u_size == y_size | u_size * 2 == y_size)) || ...
+    any(~(v_size == y_size | v_size * 2 == y_size))
+    error('YUV size NOT match!');
+end
 
 if any(u_size ~= y_size)
     u = imresize(u, y_size / 2);
