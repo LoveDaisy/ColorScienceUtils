21e68e748606bd8aac03c77e217c346c1d3c98f9
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-08-18 00:35:19 +0800

    bugfix: luv2xyz

--- matlab/+colorspace/luv2xyz.m
+++ matlab/+colorspace/luv2xyz.m
@@ -12,7 +12,7 @@ p = inputParser;
 p.addRequired('luv', @colorutil.image_shape_validator);
 p.parse(luv);
 
-w = colorspace.get_whit_point('D65');
+w = colorspace.get_white_point('D65');
 uv_n = (w * [4, 0; 0, 9; 0, 0]) / (w * [1; 15; 3]);
 
 input_size = size(luv);
@@ -22,7 +22,7 @@ uv = luv(:, 2:3) ./ luv(:, 1) / 13 + uv_n;
 xyz = zeros(size(luv));
 idx = luv(:, 1) <= 8;
 xyz(idx, 2) = w(2) * luv(idx, 1) * (3 / 29)^3;
-xyz(~idx, 2) = w(2) * ((luv(~idx, 1) + 16) / 116)^3;
+xyz(~idx, 2) = w(2) * ((luv(~idx, 1) + 16) / 116).^3;
 xyz(:, 1) = xyz(:, 2) .* uv(:, 1) ./ uv(:, 2) * 9 / 4;
 xyz(:, 3) = xyz(:, 2) .* (12 - uv * [3; 20]) ./ uv(:, 2) / 4;
 
