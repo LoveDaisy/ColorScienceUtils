a910409882f12816cd55557bbdb1df4076394ee6
Jiajie Zhang <zhangjiajie043@gmail.com>
2023-10-27 15:42:28 +0800

    bugfix: p3d65 and dcip3 trc

--- matlab/+colorspace/get_trc.m
+++ matlab/+colorspace/get_trc.m
@@ -19,7 +19,8 @@ if strcmpi(trc_name, 'Linear')
     a = 0;
     b = 0;
     k = 1.0;
-elseif strcmpi(trc_name, 'sRGB')
+elseif strcmpi(trc_name, 'sRGB') || ...
+        strcmpi(trc_name, 'p3d65') || strcmpi(trc_name, 'd65p3') ||strcmpi(trc_name, 'displayp3')
     g = 2.4;
     a = 0.055;
     b = 0.0031308;
@@ -29,14 +30,17 @@ elseif strcmpi(trc_name, 'AdobeRGB') || strcmpi(trc_name, 'ARGB')
     a = 0;
     b = 0;
     k = 0;
+elseif strcmpi(trc_name, 'p3dci') || strcmpi(trc_name, 'dci3')
+    g = 2.6;
+    a = 0;
+    b = 0;
+    k = 0;
 elseif strcmpi(trc_name, '709')
     g = 1 / 0.45;
     a = 0.099;
     b = 0.018;
     k = 4.5;
-elseif strcmpi(trc_name, '2020') || ...
-    strcmpi(trc_name, 'p3d65') || strcmpi(trc_name, 'd65p3') ||strcmpi(trc_name, 'displayp3') || ...
-    strcmpi(trc_name, 'p3dci') || strcmpi(trc_name, 'dci3')
+elseif strcmpi(trc_name, '2020')
     g = 1 / 0.45;
     a = 0.099297;
     b = 0.018053;
